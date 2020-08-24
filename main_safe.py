230f8d42164e552841748861258370bf8745d81c
Yusuf Hegazy <yusufthehegazy@gmail.com>
2020-08-24 15:31:15 +0200

    optimized

--- main_safe.py
+++ main_safe.py
@@ -1,13 +1,14 @@
 import json
 import facebook
 import requests
-#from random import randint
+from random import randint
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-#from pyvirtualdisplay import Display
+from pyvirtualdisplay import Display
+import sys
 import logging
 
 logging.basicConfig(
@@ -15,26 +16,17 @@ logging.basicConfig(
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
 
-#user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
-# display = Display(visible=0, size=(800, 600))
-# display.start()
-# options = webdriver.ChromeOptions()
-# options.add_argument("user-agent={}".format(user_agent))
-
-webdriver_path = "" #download from https://chromedriver.chromium.org/downloads
-
+fb_email = "" 
+fb_pass = ""
+token = ""
 post_id = ""
 user_id = ""
-
-fb_email = ""
-fb_pass = ""
-fb_token = "" #get access token from fb dev tools and give user_posts perm
-
-driver = webdriver.Chrome(webdriver_path)
+edge_path = ""
+driver = webdriver.Edge(edge_path)
 
 def fetch_likes_and_cmts():
 
-    graph = facebook.GraphAPI(fb_token)
+    graph = facebook.GraphAPI(token)
 
     user_post_id = "{}_{}".format(user_id, post_id)
 
@@ -53,36 +45,53 @@ def fetch_likes_and_cmts():
 
 
 def loginFn():
-    driver.get("https://m.facebook.com/")
-    email_form = driver.find_element_by_xpath('//*[@id="m_login_email"]')
-    pass_form = driver.find_element_by_xpath('//*[@id="m_login_password"]')
-    login_btn = driver.find_element_by_xpath('//*[@id="u_0_4"]/button')
+    driver.get("https://mbasic.facebook.com/")
+    email_form = driver.find_element_by_id("m_login_email")
+    pass_form = driver.find_element_by_name("pass")
+    login_btn = driver.find_element_by_name("login")
     email_form.send_keys(fb_email)
     pass_form.send_keys(fb_pass)
     login_btn.click()
     WebDriverWait(driver, 20).until(EC.title_is("Facebook"))
+    logging.info("Logged In!")
 
+old_reacts, old_cmts = 0, 0
 
 def editPostFn():
-    driver.get("https://m.facebook.com/story.php?story_fbid={}&id={}".format(post_id, user_id))
-    three_dots_xpath = "/html/body/div[1]/div/div[4]/div/div[1]/div/div/div/div[1]/header/div[2]/div/div/div[3]/div/a"
-    edit_post_btn_xpath = "/html/body/div[1]/div/div[4]/div/div[4]/div/div/div[2]/a[18]"
-    editpost_word_xpath = "/html/body/div[2]/div/div[1]/div/div[2]/div/div"
-    editpost_textbox_xpath = "/html/body/div[2]/div/div[2]/div/form/div/textarea"
-    save_btn_xpath = "/html/body/div[2]/div/div[1]/div/div[3]/div/button"
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, three_dots_xpath))).click()
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, edit_post_btn_xpath))).click()
+    global old_reacts
+    global old_cmts
+    driver.get("https://mbasic.facebook.com/story.php?story_fbid={}&id={}".format(post_id, user_id))
+    editpost_textbox_xpath = "/html/body/div/div/div[2]/div[2]/div[1]/form/textarea"
+    save_btn_xpath = "/html/body/div/div/div[2]/div[2]/div[1]/form/div/input"
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.LINK_TEXT, "Edit"))).click()
     reacts, cmts = fetch_likes_and_cmts()
-    post_text = "البوست دا فيه {} رياكت و {} كومنت".format(reacts, cmts)
-    WebDriverWait(driver, 20).until(EC.text_to_be_present_in_element((By.XPATH, editpost_word_xpath), "Edit Post"))
-    edit_form = driver.find_element_by_xpath(editpost_textbox_xpath)
-    edit_form.clear()
-    edit_form.send_keys(post_text)
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, save_btn_xpath))).click()
+
+    if (old_reacts == reacts and old_cmts == cmts):
+        logging.info("No changes! skipping this round..")
+    else:
+        post_text = "البوست دا بيحدث نفسه لوحده و فيه دلوقتي {} رياكت و {} كومنت".format(reacts, cmts)
+        maintenance = " ---- جاري صيانه البوست سنعود بعد قليل ---- "
+        WebDriverWait(driver, 20).until(EC.title_is("Edit Post"))
+        edit_form = driver.find_element_by_xpath(editpost_textbox_xpath)
+        edit_form.clear()
+        edit_form.send_keys(post_text)
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, save_btn_xpath))).click()
+        logging.info("Edited")
+    old_reacts, old_cmts = reacts, cmts
 
 
-loginFn()
+def mainLoop():
+    try:
+        loginFn()
 
-while True:
-    editPostFn()
-    time.sleep(20)
+        while True:
+            editPostFn()
+            logging.info("Sleeping for 40 Seconds........")
+            time.sleep(40)
+
+    except:
+        logging.info("OHFUCK AN EXCEPTION!.. Sleeping for 5 mins")
+        time.sleep(300)
+        mainLoop()
+
+mainLoop()
